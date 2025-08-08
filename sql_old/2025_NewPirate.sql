ec49ade362ecd939626a9ce8057a7f9cd12263c4
narakorn27 <tncyice@gmail.com>
2025-08-08 15:07:23 +0700

    ✨ sql & xml
    xml ไฟล์สำคัญที่รวมเกือบทุกอย่างในเกมที่แสดงบนเกม ทั้งภาษา และอื่นๆ ที่สำคัญต่อการตั้ง เลขต่างๆใน Btstore ไฟล์

---
+++ sql_old/2025_NewPirate.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f1c47d035f5bad18d1c0cac136045a35678757d266805c90116fd7931813a1e
+size 229053608
---
+++ sql_old/2025_t9_web.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b70b239577f4c06e953c1c29ad5235f570dd2a7686e6751937b9c44e92a9573
+size 24408
